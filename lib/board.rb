94ace490983990d79b2349307de7bbc106e46e49
Alex Nahimana <alex.nahimana1@gmail.com>
2019-07-10 15:46:30 -0500

    Add display instance method

--- lib/board.rb
+++ lib/board.rb
@@ -20,4 +20,13 @@ class Board
         cell.clear
         fix_board(cell)
     end
+
+    def display
+        # prints the board
+        puts " #{cell[0]} | #{cell[1]} | #{cell[2]} "
+        puts "-----------"
+        puts " #{cell[3]} | #{cell[4]} | #{cell[5]} "
+        puts "-----------"
+        puts " #{cell[6]} | #{cell[7]} | #{cell[8]} "
+    end
 end
