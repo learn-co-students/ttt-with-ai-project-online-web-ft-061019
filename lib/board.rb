28ca8b967a8498be08640333b451380587a2cb1e
Alex Nahimana <alex.nahimana1@gmail.com>
2019-07-10 15:42:01 -0500

    Add reset! instance method

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,23 @@
 class Board
     attr_accessor :cells
-    def initialize(cells = nil)
-         @cells ||= Array.new(9, " ")
+    def initialize()
+        @cells = Array.new(9, " ")
     end
 
     def cell
         @cells
+    end
+
+    def fix_board(board)
+        # Add empty cells
+        9.times do
+            board << " "
+        end
+    end
+
+    def reset!
+        # Reset cells
+        cell.clear
+        fix_board(cell)
     end
 end
