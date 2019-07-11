ffdd385b4eea201bafc6754ca466695e2e64384c
Alex Nahimana <alex.nahimana1@gmail.com>
2019-07-11 13:08:14 -0500

    Add play method to Game

--- lib/game.rb
+++ lib/game.rb
@@ -72,4 +72,16 @@ class Game
             end
         end
     end
+
+    def play
+        while over? == false
+          turn
+        end
+        if won?
+          puts "Congratulations #{winner}!"
+        elsif draw?
+          puts "Cat's Game!"
+        end
+    end
+
 end
